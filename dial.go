4f0840667198bc397d2517e6b6666dd1dcf8d956
Andrew M Bursavich <abursavich@gmail.com>
2014-09-22 13:45:17 -0700

    fork Dialer by go version to support go < 1.3

--- dial.go
+++ dial.go
@@ -14,57 +14,6 @@ var errTimeout = error(&timeoutError{})
 // Filter selcts IP addresses from ips.
 type Filter func(ips []net.IP) []net.IP
 
-// A Dialer contains options for connecting to an address.
-type Dialer struct {
-	// Timeout is the maximum amount of time a dial will wait for
-	// a connect to complete. If Deadline is also set, it may fail
-	// earlier.
-	//
-	// The default is no timeout.
-	//
-	// With or without a timeout, the operating system may impose
-	// its own earlier timeout. For instance, TCP timeouts are
-	// often around 3 minutes.
-	Timeout time.Duration
-
-	// Deadline is the absolute point in time after which dials
-	// will fail. If Timeout is set, it may fail earlier.
-	// Zero means no deadline, or dependent on the operating system
-	// as with the Timeout option.
-	Deadline time.Time
-
-	// LocalAddr is the local address to use when dialing an
-	// address. The address must be of a compatible type for the
-	// network being dialed.
-	//
-	// If nil, a local address is automatically chosen.
-	LocalAddr net.Addr
-
-	// Resolver is used to resolve IP addresses from domain names.
-	//
-	// If nil, DefaultResolver will be used.
-	Resolver Resolver
-
-	// Filter selects addresses from those available after
-	// resolving a host to a set of supported IPs.
-	//
-	// When dialing a TCP connection if multiple addresses are
-	// returned, then a connection will attempt to be established
-	// with each address and the first that succeeds will be returned.
-	// With any other type of connection, only the first address
-	// returned will be dialed.
-	//
-	// If nil, DefaultFilter is used.
-	Filter Filter
-
-	// KeepAlive specifies the keep-alive period for an active
-	// network connection.
-	//
-	// If zero, keep-alives are not enabled. Network protocols
-	// that do not support keep-alives ignore this field.
-	KeepAlive time.Duration
-}
-
 // Return either now+Timeout or Deadline, whichever comes first.
 // Or zero, if neither is set.
 func (d *Dialer) deadline() time.Time {
@@ -117,11 +66,7 @@ func (d *Dialer) Dial(network, address string) (net.Conn, error) {
 	if err != nil {
 		return nil, &net.OpError{Op: "dial", Net: network, Addr: nil, Err: err}
 	}
-	dialer := net.Dialer{
-		Deadline:  deadline,
-		LocalAddr: d.LocalAddr,
-		KeepAlive: d.KeepAlive,
-	}
+	dialer := d.netDialer(deadline)
 	if addrs.Len() == 1 || len(network) < 3 || network[:3] != "tcp" {
 		return dialer.Dial(network, addrs.Addr(0))
 	}
